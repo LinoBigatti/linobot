258b4e6d8429e9c0fd102d53fe749d8914e71ff5
L!no <linobigatti2015@gmail.com>
2018-12-06 13:18:16 -0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,20 @@
+import json
+
+host = input("host: ")
+
+owners = []
+
+TOKEN = input("token: ")
+
+description = input("desc: ")
+
+prefix = input("prefix: ")
+
+toogleall = 0
+
+invite_link = input("invite: ")
+
+config = {'host': host, 'owners': owners, 'token': TOKEN, 'desc': description, 'prefix': prefix, 'logall': toogleall, 'invite': invite_link}
+
+with open('config.json', 'w') as f:
+json.dump(config, f)
